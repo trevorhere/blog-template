b2fbbd57015da7776a20576dc92607396a594cbd
Trevor <trevoranthonylane@gmail.com>
2021-12-01 23:05:53 -0700

    add comments

--- services/siteData.js
+++ services/siteData.js
@@ -1,3 +1,27 @@
+// To generate SSH keys for github
+// ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+// https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+
+/*
+ Example Test Post:
+
+ ---
+ author: Trevor Lane  
+ title:  My First Post
+ date:   12/01/1985 
+ image:  https://i.imgur.com/ZBqmTXz.png
+ authorImage: https://avatars.githubusercontent.com/u/15148900
+ tags: blog, writing, post 
+ ---
+
+POST LIST EXAMPLE:
+
+[{
+   "name":"first post",
+   "gist_id": "5d0c8819cf46fea931bb089aa566d227"
+}]
+
+*/
 
 const siteData =  {
   "title": "Trevors Blog",
